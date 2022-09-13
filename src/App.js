6f1c559d5679b9518a23aa80f23cf0682b20fe67
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-13 16:08:54 +0300

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import App_css from '../src/css/App.css';
 import Header from "./Header";
+
 
 function App() {
   return (
     <div>
         <Header/>
-        <h1 className="component">Hi from App component</h1>
+        <h1 className="text-amber-800 text-3xl">Hi fromq App component</h1>
     </div>
   );
 }
