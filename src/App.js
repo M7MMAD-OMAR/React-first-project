cbe47bffea87287f1b2c33a4b650194ca53a3491
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-13 17:00:10 +0300

    change files and training

--- src/App.js
+++ src/App.js
@@ -1,13 +1,115 @@
 import Header from "./components/Header.js";
 import Card from "./components/Card.js";
+import img1 from "./images/1.png";
+import img2 from "./images/2.png";
+import img3 from "./images/3.png";
+
 
 function App() {
-  return (
-    <div>
-        <Header/>
-        <Card/>
-    </div>
-  );
+    const values = [
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'First Title',
+            img: img1,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Second Title',
+            img: img2,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+        {
+            title: 'Third Title',
+            img: img3,
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet distinctio illo, minus reiciendis repellat rerum"
+        },
+    ]
+    return (
+        <div>
+            <Header/>
+            <div className="flex flex-row  my-6 flex-wrap">
+                {
+                    values.map((item, index) => {
+                        return <Card title={item.title} img={item.img} body={item.body}/>;
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
