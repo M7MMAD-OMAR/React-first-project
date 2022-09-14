6576d1fb5d74f01c6c561983f1461e01971206c6
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-14 19:04:09 +0300

    change array and add in array

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import img1 from "../images/1.png";
 import img2 from "../images/2.png";
 import img3 from "../images/3.png";
 import Card from "./Card";
-import {createContext, useContext, useEffect, useRef, useState} from "react";
+import {createContext, useContext, useEffect, useMemo, useRef, useState} from "react";
 import {UsersList} from "./context/Users";
 
 
@@ -27,41 +27,59 @@ const Home = () => {
     const {users, add_users} = useContext(UsersList);
     const [first_name, setFirst_name] = useState('');
     const [last_name, setLast_name] = useState('');
-    let temps = []
+
+    // temps = JSON.parse(localStorage.getItem('temps') || '[]')
+    let [temps, setTemps] = useState(JSON.parse(localStorage.getItem('temps') || '[]'));
     const print_data = () => {
-            temps = JSON.parse(localStorage.getItem('temps') || '[]')
-            let temp = {
-                first_name: first_name,
-                last_name: last_name,
-            }
-            temps.push(temp);
-            localStorage.setItem('temps', JSON.stringify(temps));
+        let temp = {
+            first_name: first_name,
+            last_name: last_name,
+        }
+        temps.push(temp)
+        setTemps(temp);
+        localStorage.setItem('temps', JSON.stringify(temps));
+        console.log(temps.length)
+
 
     }
 
 
     return (
         <>
-            {users}
+            <table>
+                <tr>
+                    <td>#</td>
+                    <td>First Name</td>
+                    <td>last Name</td>
+                </tr>
+                {temps.map((item, key) => {
+                    return <tr key={key}>
+                        <td>{key}</td>
+                        <td>{item.first_name}</td>
+                        <td>{item.last_name}</td>
+                    </tr>
+                })}
+            </table>
+
             <div className='container'>
                 <label htmlFor="first_name">
                     <input type="text" placeholder='First Name' name='first_name' id='first_name'
-                          onChange={(e)=>{setFirst_name(e.target.value)}} />
+                           onChange={(e) => {
+                               setFirst_name(e.target.value)
+                           }}/>
                 </label>
                 <br/>
                 <br/>
                 <label htmlFor="last_name">
                     <input type="text" placeholder='Last Name' name='last_name' id='last_name'
-                           onChange={(e)=>{setLast_name(e.target.value)}} />
+                           onChange={(e) => {
+                               setLast_name(e.target.value)
+                           }}/>
                 </label>
                 <br/><br/>
                 <button value='Submit' onClick={print_data}>Submit</button>
             </div>
-            <div className="flex flex-row  my-6 flex-wrap">
-                {
-                   console.log(temps)
-                }
-            </div>
+
             {/* show all cards start */}
             <div className="flex flex-row  my-6 flex-wrap">
                 {
