4c2f8322ded17a619f51e9ac854b44e9328f61bc
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-18 14:15:10 +0300

    change files

--- src/components/build/Actions.js
+++ src/components/build/Actions.js
@@ -1,12 +1,12 @@
 
 export const Actions = (props) => {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div key={props.key} className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">{props.title}</h2>
                 <p>{props.body}</p>
             </div>
-            {/*<figure><img src="https://placeimg.com/640/480/animals/grayscale" alt="Shoes" /></figure>*/}
+            <figure><img src="https://placeimg.com/640/480/animals/grayscale" alt="Shoes" /></figure>
         </div>
     )
 
