47cefda236b78ffb7665f7bc34b8c50c9cc8551f
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-09-21 13:46:39 +0300

    create method and use redux

--- src/reducer/reducerCounter.js
+++ src/reducer/reducerCounter.js
@@ -1,11 +1,11 @@
-
+import {DECREMENT, INCREMENT} from "../publicParameters/parameters";
 
 
 export const reducerCounter = (state = {counter: 0}, action)=>{
     switch (action.type) {
-        case '+':
+        case INCREMENT:
             return {counter: state.counter + 1}
-        case '-':
+        case DECREMENT:
             return {counter: state.counter - 1}
         default:
             return {counter: state.counter = 0}
