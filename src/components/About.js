eaa32d76a40af93381f0e61daff58c0f7055cb4d
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-09-22 11:24:30 +0300

    change formatter

--- src/components/About.js
+++ src/components/About.js
@@ -2,40 +2,39 @@ import {useReducer} from "react";
 import ChangeNumber from "./reducers/ChangeNumber";
 
 
-
 const About = () => {
-    const numbers = {
-        counter: 0,
-    }
-    const [counter_state, counter_dispatch] = useReducer(ChangeNumber, numbers);
+	const numbers = {
+		counter: 0,
+	}
+	const [counter_state, counter_dispatch] = useReducer(ChangeNumber, numbers);
 
-    return (
+	return (
 
-        <div className="min-h-screen">
-            <button className="btn btn-circle" onClick={() => {
-                counter_dispatch({type: '+'})
-            }}>
-                <p>+</p>
-            </button>
+		<div className="min-h-screen">
+			<button className="btn btn-circle" onClick={() => {
+				counter_dispatch({type: '+'})
+			}}>
+				<p>+</p>
+			</button>
 
-            <button className="btn btn-circle" onClick={() => {
-                counter_dispatch({type: '-'})
-            }}>
-                <p>-</p>
-            </button>
+			<button className="btn btn-circle" onClick={() => {
+				counter_dispatch({type: '-'})
+			}}>
+				<p>-</p>
+			</button>
 
-            <button className="btn btn-circle w-fit p-3" onClick={() => {
-                counter_dispatch({type: '0'})
-            }}>
-                <p>Clear</p>
-            </button>
+			<button className="btn btn-circle w-fit p-3" onClick={() => {
+				counter_dispatch({type: '0'})
+			}}>
+				<p>Clear</p>
+			</button>
 
-            <br/><br/>
-            <button className="btn btn-circle">
-                <p>{counter_state.counter}</p>
-            </button>
-        </div>
-    );
+			<br/><br/>
+			<button className="btn btn-circle">
+				<p>{counter_state.counter}</p>
+			</button>
+		</div>
+	);
 }
 
 export default About;
