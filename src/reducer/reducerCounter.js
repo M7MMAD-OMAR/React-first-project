8bacb5bcbb2412c72895a97a795c0c658c0d086d
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-09-22 11:23:54 +0300

    change formatter

--- src/reducer/reducerCounter.js
+++ src/reducer/reducerCounter.js
@@ -1,13 +1,13 @@
 import {DECREMENT, INCREMENT} from "../publicParameters/parameters";
 
 
-export const reducerCounter = (state = {counter: 0}, action)=>{
-    switch (action.type) {
-        case INCREMENT:
-            return {counter: state.counter + 1}
-        case DECREMENT:
-            return {counter: state.counter - 1}
-        default:
-            return {counter: state.counter = 0}
-    }
+export const reducerCounter = (state = {counter: 0}, action) => {
+	switch (action.type) {
+		case INCREMENT:
+			return {counter: state.counter + 1}
+		case DECREMENT:
+			return {counter: state.counter - 1}
+		default:
+			return {counter: state.counter = 0}
+	}
 }
