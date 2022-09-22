dda1e901714df07a73e6e8f52c6ef803b8a60b06
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-09-22 11:24:28 +0300

    change formatter

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,71 +2,70 @@ import {useState} from "react";
 import {motion, Variants} from "framer-motion";
 
 const itemVariants: Variants = {
-    open: {
-        opacity: 1,
-        y: 0,
-        transition: {type: "spring", stiffness: 300, damping: 24}
-    },
-    closed: {opacity: 0, y: 20, transition: {duration: 0.2}}
+	open: {
+		opacity: 1,
+		y: 0,
+		transition: {type: "spring", stiffness: 300, damping: 24}
+	},
+	closed: {opacity: 0, y: 20, transition: {duration: 0.2}}
 };
 
 
-
 export default function Content() {
-    const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-    return (
+	return (
 
-        <motion.nav
-            initial={false}
-            animate={isOpen ? "open" : "closed"}
-            className="menu">
-            <motion.button
-                whileTap={{scale: 0.97}}
-                onClick={() => setIsOpen(!isOpen)}
-                className="bg-blue-900 w-1/3 rounded-full flex flex-row justify-center py-2">
-                <span className="mx-3">Menu</span>
-                <motion.div
-                    variants={{
-                        open: {rotate: 180},
-                        closed: {rotate: 0}
-                    }}
-                    transition={{duration: 0.2}}
-                    style={{originY: 0.55, marginTop: 6}}>
-                    <svg width="15" height="15" viewBox="0 0 20 20">
-                        <path d="M0 7 L 20 7 L 10 16"/>
-                    </svg>
-                </motion.div>
-            </motion.button>
-            <motion.ul
-                variants={{
-                    open: {
-                        clipPath: "inset(0% 0% 0% 0% round 10px)",
-                        transition: {
-                            type: "spring",
-                            bounce: 0,
-                            duration: 0.7,
-                            delayChildren: 0.3,
-                            staggerChildren: 0.05
-                        }
-                    },
-                    closed: {
-                        clipPath: "inset(10% 50% 90% 50% round 10px)",
-                        transition: {
-                            type: "spring",
-                            bounce: 0,
-                            duration: 0.3
-                        }
-                    }
-                }}
-                style={{pointerEvents: isOpen ? "auto" : "none"}}
-                className="bg-slate-400 text-black px-4 rounded-sm w-1/3">
-                <motion.li variants={itemVariants} className="py-1 hover:bg-slate-500">Item 1</motion.li>
-                <motion.li variants={itemVariants}>First 2</motion.li>
-                <motion.li variants={itemVariants}>Second 3</motion.li>
-                <motion.li variants={itemVariants}>Item 4</motion.li>
-                <motion.li variants={itemVariants}>Item 5</motion.li>
-            </motion.ul>
-        </motion.nav>
-    );
+		<motion.nav
+			initial={false}
+			animate={isOpen ? "open" : "closed"}
+			className="menu">
+			<motion.button
+				whileTap={{scale: 0.97}}
+				onClick={() => setIsOpen(!isOpen)}
+				className="bg-blue-900 w-1/3 rounded-full flex flex-row justify-center py-2">
+				<span className="mx-3">Menu</span>
+				<motion.div
+					variants={{
+						open: {rotate: 180},
+						closed: {rotate: 0}
+					}}
+					transition={{duration: 0.2}}
+					style={{originY: 0.55, marginTop: 6}}>
+					<svg width="15" height="15" viewBox="0 0 20 20">
+						<path d="M0 7 L 20 7 L 10 16"/>
+					</svg>
+				</motion.div>
+			</motion.button>
+			<motion.ul
+				variants={{
+					open: {
+						clipPath: "inset(0% 0% 0% 0% round 10px)",
+						transition: {
+							type: "spring",
+							bounce: 0,
+							duration: 0.7,
+							delayChildren: 0.3,
+							staggerChildren: 0.05
+						}
+					},
+					closed: {
+						clipPath: "inset(10% 50% 90% 50% round 10px)",
+						transition: {
+							type: "spring",
+							bounce: 0,
+							duration: 0.3
+						}
+					}
+				}}
+				style={{pointerEvents: isOpen ? "auto" : "none"}}
+				className="bg-slate-400 text-black px-4 rounded-sm w-1/3">
+				<motion.li variants={itemVariants} className="py-1 hover:bg-slate-500">Item 1</motion.li>
+				<motion.li variants={itemVariants}>First 2</motion.li>
+				<motion.li variants={itemVariants}>Second 3</motion.li>
+				<motion.li variants={itemVariants}>Item 4</motion.li>
+				<motion.li variants={itemVariants}>Item 5</motion.li>
+			</motion.ul>
+		</motion.nav>
+	);
 }
